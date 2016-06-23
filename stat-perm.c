a25933ea8d206fe7944d116907ddcfc1f780e3a5
vmm0 <vmm@null.net>
2016-06-23 22:06:01 +0200

    Create stat-perm.c

---
+++ stat-perm.c
@@ -0,0 +1,18 @@
+//Determine File Owner`s Write Permission
+//e.g.- ./stat-perm program
+
+#include <stdio.h>
+#include <sys/stat.h>
+
+int main (int argc, char* argv[])
+{
+	const char* const filename = argv[1];
+	struct stat buf;
+
+	/* Get file information. */
+	stat (filename, &buf);
+	/* If the permissions are set such that the file`s owner can write to it */
+	if (buf.st_mode & S_IWUSR)
+		printf ("Owning user can write '%s'.\n", filename);
+	return 0;
+}
